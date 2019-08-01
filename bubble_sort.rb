a5a87cf80c39e5b187a3685de5f29997aa1dfe39
Eva <evaveskova@gmail.com>
2019-08-01 17:18:03 +0200

    bubble sort method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,1 +1,19 @@
+test = [3, 18, 17, 28, 2]
 
+def bubble_sort(array)
+  arr = array.length
+
+  loop do
+    action = false
+    (arr-1).times do |i|
+      if array[i] > array[i + 1]
+        array[i], array[i + 1] = array[i + 1], array[i]
+        action = true
+      end
+  end
+
+    break if not action
+  end
+
+  return array
+end
