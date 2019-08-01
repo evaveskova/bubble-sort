f183d9eca65a8bc8a7c934f9e3ab433e454b5fd5
Eva <evaveskova@gmail.com>
2019-08-01 17:35:52 +0200

    bubble sort by

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -17,3 +17,21 @@ def bubble_sort(array)
 
   return array
 end
+
+
+def bubble_sort_by(array)
+  switch = true
+  while switch
+    for i in (0...array.length-1)
+    result = yield([array[i], array[i + 1]])
+    if result > 0
+      array[i], array[i + 1] = array[i + 1], array[i]
+    else switch = false
+    end
+    end
+  end
+end
+
+bubble_sort_by(["hi","hello","hey"]) do |left,right|
+   left.length - right.length
+ end
